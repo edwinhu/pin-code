64230fa41e78551631c46142301caa5c42be2edc
Edwin Hu <edwinhu@edwins-air.lan>
2021-01-06 11:37:38 -0500

    For posting

--- run_est.sh
+++ run_est.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #$ -cwd
 #$ -m abe
-#$ -pe onenode 4
+#$ -pe onenode 8
+#$ -l m_mem_free=3G
 #$ -M eddyhu@gmail.com
 model=$1;shift
 year=$1;shift
 source ~/miniconda3/bin/activate
-ipcluster start -n 7 --cluster-id="$model-$year" &
+ipcluster start -n 15 --cluster-id="$model-$year" &
 sleep 45
 ipython est.py $model $year
 ipcluster stop
